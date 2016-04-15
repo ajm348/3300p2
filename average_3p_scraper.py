1a259c32b4039a04744754d0d07615e58330d8b1
Adam Mahar <ajm348@cornell.edu>
2016-04-15 00:11:04 -0400

    added drafts data

--- average_3p_scraper.py
+++ average_3p_scraper.py
@@ -1,17 +1,6 @@
 from urllib2 import urlopen
 from bs4 import BeautifulSoup
 
-#THIS SCRIPT NOT IMPLEMENTED, JUST PLACEHOLDING AS A TODO
-
-#url of page you want to parse (in this case, list of all NBA champs)
-# leagues_url='http://www.basketball-reference.com/leagues/'
-# #turn it into "beautiful soup"
-# leagues_soup = BeautifulSoup(urlopen(champions_url), "html.parser")
-# #find table within page, either by class or id or both
-# leagues_table = champions_soup.find('table', attrs={'class': ' stats_table'})
-# #http://stackoverflow.com/questions/2010481/how-do-you-get-all-the-rows-from-a-particular-table-using-beautifulsoup
-# #find all the rows (trs) in the table
-# league_rows = leagues_table.findChildren(["tr"])
 #header row for eventual printing stuff
 print "Year,avg_3PA,avg_3P%"
 for i in range(2016,1977,-1):
